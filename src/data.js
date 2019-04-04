a4ec34684681c30ef0bbf11d44cf0cafca41cef9
Facundo HM <facundo.hmz@gmail.com>
2019-04-03 23:18:32 -0300

    Add src folder and create data file

---
+++ src/data.js
@@ -0,0 +1,55 @@
+exports.grandTours = [
+  {
+    id: '1',
+    name: "Giro d'Italia",
+    date: 'May–June',
+    region: 'Italy',
+    firstEdition: 1909,
+    editions: 101,
+    jerseyColor: 'Pink'
+  },
+  {
+    id: '2',
+    name: 'Tour de France',
+    date: 'July',
+    region: 'France',
+    firstEdition: 1903,
+    editions: 105,
+    jerseyColor: 'Yellow'
+  },
+  {
+    id: '3',
+    name: 'Vuelta a España',
+    date: 'August–September',
+    region: 'Spain',
+    firstEdition: 1935,
+    editions: 73,
+    jerseyColor: 'Red'
+  }
+]
+
+exports.riders = [
+  { id: '1', name: 'Chris Froome', country: 'Great Britain' },
+  { id: '2', name: 'Geraint Thomas', country: 'Great Britain' },
+  { id: '3', name: 'Simon Yates', country: 'Great Britain' },
+  { id: '4', name: 'Tom Dumoulin', country: 'Netherlands' },
+  { id: '5', name: 'Vincenzo Nibali', country: 'Italy' },
+  { id: '6', name: 'Nairo Quintana', country: 'Colombia' },
+  { id: '7', name: 'Alberto Contador', country: 'Spain' },
+  { id: '8', name: 'Fabio Aru', country: 'Italy' }
+]
+
+exports.editions = [
+  { id: '1', year: 2015, winnerId: '7', tourId: '1' },
+  { id: '2', year: 2015, winnerId: '1', tourId: '2' },
+  { id: '3', year: 2015, winnerId: '8', tourId: '3' },
+  { id: '4', year: 2016, winnerId: '5', tourId: '1' },
+  { id: '5', year: 2016, winnerId: '1', tourId: '2' },
+  { id: '6', year: 2016, winnerId: '6', tourId: '3' },
+  { id: '7', year: 2017, winnerId: '4', tourId: '1' },
+  { id: '8', year: 2017, winnerId: '1', tourId: '2' },
+  { id: '9', year: 2017, winnerId: '1', tourId: '3' },
+  { id: '10', year: 2018, winnerId: '1', tourId: '1' },
+  { id: '11', year: 2018, winnerId: '2', tourId: '2' },
+  { id: '12', year: 2018, winnerId: '3', tourId: '3' }
+]
--- src/schema.js
+++ src/schema.js
@@ -1,5 +1,3 @@
-const graphql = require('graphql')
-
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -8,63 +6,9 @@ const {
   GraphQLList,
   GraphQLInt,
   GraphQLNonNull
-} = graphql
+} = require('graphql')
 
-const grandTours = [
-  {
-    id: '1',
-    name: "Giro d'Italia",
-    date: 'May–June',
-    region: 'Italy',
-    firstEdition: 1909,
-    editions: 101,
-    jerseyColor: 'Pink'
-  },
-  {
-    id: '2',
-    name: 'Tour de France',
-    date: 'July',
-    region: 'France',
-    firstEdition: 1903,
-    editions: 105,
-    jerseyColor: 'Yellow'
-  },
-  {
-    id: '3',
-    name: 'Vuelta a España',
-    date: 'August–September',
-    region: 'Spain',
-    firstEdition: 1935,
-    editions: 73,
-    jerseyColor: 'Red'
-  }
-]
-
-const riders = [
-  { id: '1', name: 'Chris Froome', country: 'Great Britain' },
-  { id: '2', name: 'Geraint Thomas', country: 'Great Britain' },
-  { id: '3', name: 'Simon Yates', country: 'Great Britain' },
-  { id: '4', name: 'Tom Dumoulin', country: 'Netherlands' },
-  { id: '5', name: 'Vincenzo Nibali', country: 'Italy' },
-  { id: '6', name: 'Nairo Quintana', country: 'Colombia' },
-  { id: '7', name: 'Alberto Contador', country: 'Spain' },
-  { id: '8', name: 'Fabio Aru', country: 'Italy' }
-]
-
-const editions = [
-  { id: '1', year: 2015, winnerId: '7', tourId: '1' },
-  { id: '2', year: 2015, winnerId: '1', tourId: '2' },
-  { id: '3', year: 2015, winnerId: '8', tourId: '3' },
-  { id: '4', year: 2016, winnerId: '5', tourId: '1' },
-  { id: '5', year: 2016, winnerId: '1', tourId: '2' },
-  { id: '6', year: 2016, winnerId: '6', tourId: '3' },
-  { id: '7', year: 2017, winnerId: '4', tourId: '1' },
-  { id: '8', year: 2017, winnerId: '1', tourId: '2' },
-  { id: '9', year: 2017, winnerId: '1', tourId: '3' },
-  { id: '10', year: 2018, winnerId: '1', tourId: '1' },
-  { id: '11', year: 2018, winnerId: '2', tourId: '2' },
-  { id: '12', year: 2018, winnerId: '3', tourId: '3' }
-]
+const { grandTours, riders, editions } = require('./data')
 
 const GrandTourType = new GraphQLObjectType({
   name: 'GrandTour',
